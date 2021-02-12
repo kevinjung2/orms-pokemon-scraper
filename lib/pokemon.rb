937caa2c947bd8468c8a3de0be04dfc76b7fc4f2
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-12 11:35:56 -0800

    commit tree 6329552f28c250830bb30e214bbb4b68f6109bb9

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,25 @@
 class Pokemon
+  attr_accessor :name, :type, :id, :db
+  def initialize(id: nil, name:, type:, db:)
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+  end
+
+  def self.save (name, type, db)
+    new = self.new(name: name,type: type,db: db)
+
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type) VALUES (?, ?)
+    SQL
+    db.execute(sql, new.name, new.type)
+    new.id = db.execute("SELECT last_insert_rowid() FROM pokemon")
+  end
+
+  def self.find(id, db)
+    db.execute("SELECT * FROM pokemon WHERE id = ?", id).map do |row|
+      self.new(id: id, name: row[1], type: row[2], db: db)
+    end.first
+  end
 end
